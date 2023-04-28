686bd2561d7f41abe2d437c4a6c1573bf63fbbb9
Manu <116085483+theperk08@users.noreply.github.com>
2023-04-28 11:11:27 +0200

    Update app_cine.py

--- app_cine.py
+++ app_cine.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import numpy as np
 
+df = pd.read_pickle("df_noms_films.pkl.gz", compression = 'gzip')
+
+
+
 st.set_page_config(
   page_title = "Ex-stream-ly Cool App",
   layout = "wide",

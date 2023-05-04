3615eb4dfa17ff73d66f26acc6d4d45fe6574c4c
Manu <116085483+theperk08@users.noreply.github.com>
2023-05-04 14:54:10 +0200

    Update app_cine.py

--- app_cine.py
+++ app_cine.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neighbors import NearestNeighbors
 
 # import des fichiers

f0963314f48ed1b699b30e7310a78782dfb98589
Manu <116085483+theperk08@users.noreply.github.com>
2023-05-24 11:29:16 +0200

    Update app_cine.py

--- app_cine.py
+++ app_cine.py
@@ -27,11 +27,12 @@ print('debut')
 
 df_annees = df_final[['tconst', 'startYear']]
 
-scaling = MinMaxScaler()
-scaling.fit(df_final[['startYear', 'runtimeMinutes', 'averageRating', 'numVotes']])
+#scaling = MinMaxScaler()
+#scaling.fit(df_final[['startYear', 'runtimeMinutes', 'averageRating', 'numVotes']])
 
 #df_final[['startYear', 'runtimeMinutes', 'averageRating', 'numVotes']] = StandardScaler().fit_transform(df_final[['startYear', 'runtimeMinutes', 'averageRating', 'numVotes']])
-df_final[['startYear', 'runtimeMinutes', 'averageRating', 'numVotes']] = scaling.transform(df_final[['startYear', 'runtimeMinutes', 'averageRating', 'numVotes']])
+df_final[['startYear', 'runtimeMinutes', 'averageRating', 'numVotes']] = MinMaxScaler().fit_transform(df_final[['startYear', 'runtimeMinutes', 'averageRating', 'numVotes']])
+#df_final[['startYear', 'runtimeMinutes', 'averageRating', 'numVotes']] = scaling.transform(df_final[['startYear', 'runtimeMinutes', 'averageRating', 'numVotes']])
 
 df_test = df_final.iloc[:, 4:]
 
